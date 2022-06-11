59aa9e8972fc73a06fed2bb97156828f8a6ba229
Kitenite <hoakiet98@gmail.com>
2022-06-11 16:33:18 -0500

    Add idleness detection

--- screenshot.py
+++ screenshot.py
@@ -6,12 +6,23 @@ import time
 
 folder_path = f"{os.path.dirname(os.path.abspath(__file__))}/images"
 compression_rate = 20
-wait_time_seconds = 10
+wait_time_seconds = 5
+previous_image = None
+
+def save_image(image):
+    file_name = f"{folder_path}/{datetime.now()}.png"
+    image.save(file_name, 'png')
 
 while True:
-    im = ImageGrab.grab()
-    fname = f"{folder_path}/{datetime.now()}.png"
-    (width, height) = (im.width // compression_rate, im.height // compression_rate)
-    im = im.resize((width, height))
-    im.save(fname, 'png')
+    current_image = ImageGrab.grab()
+    (width, height) = (current_image.width // compression_rate, current_image.height // compression_rate)
+    current_image = current_image.resize((width, height))
+
+    if not previous_image:
+        save_image(current_image)
+    elif list(current_image.getdata()) != list(previous_image.getdata()):
+        save_image(current_image)
+    # else is idle
+
+    previous_image = current_image
     time.sleep(wait_time_seconds)
